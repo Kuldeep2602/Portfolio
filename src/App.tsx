795780348c25c04b61d1ccb2bd09f027d8e7faaf
Kuldeep <e22cseu1637@bennett.edu.in>
2025-11-24 23:07:19 +0530

    tilt improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import LandingPage from './pages/LandingPage';
@@ -10,9 +11,13 @@ import ThemeToggle from './components/ThemeToggle';
 function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
-        <ThemeToggle />
-        <main className="flex-grow px-4 md:px-8 max-w-7xl mx-auto w-full">
+      <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 transition-colors duration-300 relative">
+        {/* Theme Toggle */}
+        <div className="fixed top-4 right-4 md:top-6 md:right-6 z-50">
+          <ThemeToggle />
+        </div>
+
+        <main className="flex-grow px-4 md:px-8 max-w-7xl mx-auto w-full relative z-10">
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/" element={<LandingPage />} />
